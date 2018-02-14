3b8d20c4a22cf54ed7b0f56d0e68bacea4492b7f
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-02-14 15:08:00 -0500

    Creates a TimerButton component for showing a start or stop button.

--- src/App.js
+++ src/App.js
@@ -26,4 +26,28 @@ class Timer extends React.Component {
   }
 }
 
+class TimerButton extends React.Component {
+  render() {
+    if (this.props.timerIsRunning) {
+      return (
+        <div
+          className=''
+          onClick={this.props.onStopClick}
+        >
+          Stop
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className=''
+          onClick={this.props.onStartClick}
+        >
+          Start
+        </div>
+      );
+    }
+  }
+}
+
 export default App;
