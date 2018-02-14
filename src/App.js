d66042e69422a9b4be76b28382a1ff83e03fa57c
meingorn <max.eingorn@cloudreach.com>
2018-02-14 15:36:03 -0500

    move timer to separate file, create functional timer in react

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Timer from './Timer.js'
 
 class App extends Component {
   render() {
     return <Timer />;
-  }
-}
-
-class Timer extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2
-          className=''
-        >
-          Timer
-        </h2>
-        <TimerButton
-          onStartClick={this.handleStartClick}
-          onStopClick={this.handleStopClick}
-        />
-      </div>
-    );
-  }
-}
-
-class TimerButton extends React.Component {
-  render() {
-    if (this.props.timerIsRunning) {
-      return (
-        <div
-          className=''
-          onClick={this.props.onStopClick}
-        >
-          Stop
-        </div>
-      );
-    } else {
-      return (
-        <div
-          className=''
-          onClick={this.props.onStartClick}
-        >
-          Start
-        </div>
-      );
-    }
   }
 }
 
