4a0b7f4d3a6960e17bb3b26d72ec07a73943950d
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-02-14 15:47:05 -0500

    Fixes formatting of file.

--- src/Timer.js
+++ src/Timer.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Timer extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.tick = this.tick.bind(this);
     this.startTimer = this.startTimer.bind(this);
@@ -16,38 +15,34 @@ export default class Timer extends Component {
   };
 
   startTimer() {
-      let timer = setInterval(this.tick, 1000);
-      this.setState({timer});
-    }
+    let timer = setInterval(this.tick, 1000);
+    this.setState({ timer });
+  }
 
   pauseTimer() {
     clearInterval(this.state.timer);
-    }
+  }
 
   clearTimer() {
     clearInterval(this.state.timer);
-    this.setState({ counter: 0});
-    }
+    this.setState({ counter: 0 });
+  }
 
   tick() {
-      this.setState({
-        counter: this.state.counter + 1
-      });
-    }
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
 
   render() {
     return (
       <div>
-        <h2
-          className=''
-        >
-          Timer
-        </h2>
+        <h2 className="">Timer</h2>
         <div>{this.state.counter}</div>
         <div>
-        <button onClick={this.startTimer}>Start</button>
-        <button onClick={this.pauseTimer}>Pause</button>
-        <button onClick={this.clearTimer}>Clear</button>
+          <button onClick={this.startTimer}>Start</button>
+          <button onClick={this.pauseTimer}>Pause</button>
+          <button onClick={this.clearTimer}>Clear</button>
         </div>
       </div>
     );
