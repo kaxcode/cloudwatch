a4ea4c42db1f93c852bf4c69253d5ebe07d8f465
meingorn <max.eingorn@cloudreach.com>
2018-02-22 13:52:38 -0500

    add file for timer testing

---
+++ src/Timer.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import Timer from './Timer';
+// import TestRenderer from 'react-test-renderer';
+//
+// jest.useFakeTimers();
+// Date.now = jest.fn(() => 1482363367071);
+//
+// it('renders correctly', () => {
+//   const tree = renderer.create(<Timer />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
