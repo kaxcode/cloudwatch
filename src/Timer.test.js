7f3525314a7e760000e4735d1671d9f62a9dda7a
meingorn <max.eingorn@cloudreach.com>
2018-02-23 10:46:05 -0500

    Add snapshot test

--- src/Timer.test.js
+++ src/Timer.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import Timer from './Timer';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders correctly', () => {
+
+  jest.useFakeTimers();
+
+  const tree = renderer.create(<Timer />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
