bb1f93d27a6a96f5ac93f3ae52ea5b8d59080175
meingorn <max.eingorn@cloudreach.com>
2018-02-28 10:41:51 -0500

    Add test for timer's fucntionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Timer from './Timer';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders correctly', () => {
-
-  jest.useFakeTimers();
-
   const tree = renderer.create(<Timer />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+jest.useFakeTimers();
+
+test('Timer runs after click', () => {
+  const timer = shallow(<Timer />);
+  timer.instance().startTimer();
+
+  expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
 });
