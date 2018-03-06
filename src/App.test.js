22a13ee45ff32402d4083802739953ff28efdf85
meingorn <max.eingorn@cloudreach.com>
2018-03-06 09:44:03 -0500

    Update branch

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import Stopwatch from './Stopwatch';
-import renderer from 'react-test-renderer';
-import {shallow} from 'enzyme';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Stopwatch />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-jest.useFakeTimers();
-
-test('Stopwatch runs after click', () => {
-  const stopwatch = shallow(<Stopwatch />);
-  stopwatch.instance().startStopwatch();
-
-  expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
