1f7a6db701a00e3d63689fb3d8aee2da4817b424
Kenia <keniavalladarez@gmail.com>
2018-03-15 15:26:41 -0400

    Fixes the descriptions and groups some of the test and assertions.

--- src/utils/humanizeTimer.test.js
+++ src/utils/humanizeTimer.test.js
@@ -1,43 +1,37 @@
 import { secondsToHour, secondsToMinutes, seconds } from './humanizeTimer';
 
-describe('Timer helpers', () => {
-  it('it should return seconds in hours', () => {
-    // Arrange & Act
-    const actual = secondsToHour(82800);
+describe('humanizeTimer', () => {
+  describe('#secondsToHour', () => {
+    it('converts seconds to a rounded-down whole hour', () => {
+      // Arrange & Act
+      const actual = secondsToHour(86350);
 
-    // Assert
-    expect(actual).toEqual(23);
+      // Assert
+      expect(actual).toEqual(23);
+      expect(actual).not.toBe(86350);
+      expect(actual).not.toBe(24);
+    });
   });
 
-  it('it should return seconds in hours rounded down', () => {
-    // Arrange & Act
-    const actual = secondsToHour(86350);
+  describe('#secondsToMinutes', () => {
+    it('converts seconds to a rounded-down whole minute', () => {
+      // Arrange & Act
+      const actual = secondsToMinutes(3430);
 
-    // Assert
-    expect(actual).toEqual(23);
+      // Assert
+      expect(actual).toEqual(57);
+      expect(actual).not.toBe(3430);
+      expect(actual).not.toBe(58);
+    });
   });
 
-  it('it should return seconds in minutes', () => {
-    // Arrange & Act
-    const actual = secondsToMinutes(180);
+  describe('#seconds', () => {
+    it('it should return seconds rounded down', () => {
+      // Arrange & Act
+      const actual = seconds(34.7898);
 
-    // Assert
-    expect(actual).toEqual(3);
-  });
-
-  it('it should return seconds in minutes rounded down', () => {
-    // Arrange & Act
-    const actual = secondsToMinutes(3430);
-
-    // Assert
-    expect(actual).toEqual(57);
-  });
-
-  it('it should return seconds rounded down', () => {
-    // Arrange & Act
-    const actual = seconds(34.7898);
-
-    // Assert
-    expect(actual).toEqual(34);
+      // Assert
+      expect(actual).toEqual(34);
+    });
   });
 });
