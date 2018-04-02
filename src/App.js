768a229c771bece54cef6bb6967f2298dc54e21b
meingorn <max.eingorn@cloudreach.com>
2018-04-02 17:04:47 -0400

    Add Cloud svgs, make them app home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import StopwatchContainer from './components/Stopwatch/StopwatchContainer.js';
 import TimerContainer from './components/Timer/TimerContainer.js';
-import { Row } from 'react-materialize';
+import { Row, Col } from 'react-materialize';
+import CloudyTimerButton from './assets/cloudy_timer_button.svg';
+import CloudyStopwatchButton from './assets/cloudy_stopwatch_button.svg';
 
 class App extends React.Component {
+  state = {
+    timerClicked: false,
+    stopwatchClicked: false
+  }
+
+  handleTimerClick = (e) => {
+    e.preventDefault();
+    this.setState({ timerClicked: true });
+  }
+
+  handleStopwatchClick = (e) => {
+    e.preventDefault();
+    this.setState({ stopwatchClicked: true });
+  }
+
   render() {
-    return (
-      <Row>
-        <StopwatchContainer />
-        <TimerContainer />
-      </Row>
-    );
+    if(!this.state.timerClicked && !this.state.stopWatchClicked) {
+      return <Row>
+                <Col onClick={this.handleTimerClick}>
+                  <img src={CloudyTimerButton} />
+                </Col>
+                <Col onClick={this.handleStopwatchClick}>
+                  <img src={CloudyStopwatchButton} />
+                </Col>
+              </Row>;
+    } else if(this.state.timerClicked && !this.state.stopwatchClicked ) {
+      return <TimerContainer />;
+    } else if(this.state.stopwatchClicked && !this.state.timerClicked) {
+      return <StopwatchContainer />;
+    } else {
+      return <div><h4>Something went wrong. Please reload the page</h4></div>;
+    }
   }
 }
 
