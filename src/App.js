6f7162829444081d6c9f3e55782f4d56e1cef012
meingorn <max.eingorn@cloudreach.com>
2018-04-03 09:56:27 -0400

    Implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import StopwatchContainer from './components/Stopwatch/StopwatchContainer.js';
 import TimerContainer from './components/Timer/TimerContainer.js';
-import { Row, Col } from 'react-materialize';
+import { Row } from 'react-materialize';
 import CloudyTimerButton from './assets/cloudy_timer_button.svg';
 import CloudyStopwatchButton from './assets/cloudy_stopwatch_button.svg';
 
-class App extends React.Component {
-  state = {
-    timerClicked: false,
-    stopwatchClicked: false
-  }
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component ={Home} />
+      <Route path="/timer" component ={TimerContainer} />
+      <Route path="/stopwatch" component ={StopwatchContainer} />
+    </div>
+  </Router>
+);
 
-  handleTimerClick = (e) => {
-    e.preventDefault();
-    this.setState({ timerClicked: true });
-  }
-
-  handleStopwatchClick = (e) => {
-    e.preventDefault();
-    this.setState({ stopwatchClicked: true });
-  }
-
-  render() {
-    if(!this.state.timerClicked && !this.state.stopWatchClicked) {
-      return <Row>
-                <Col onClick={this.handleTimerClick}>
-                  <img src={CloudyTimerButton} />
-                </Col>
-                <Col onClick={this.handleStopwatchClick}>
-                  <img src={CloudyStopwatchButton} />
-                </Col>
-              </Row>;
-    } else if(this.state.timerClicked && !this.state.stopwatchClicked ) {
-      return <TimerContainer />;
-    } else if(this.state.stopwatchClicked && !this.state.timerClicked) {
-      return <StopwatchContainer />;
-    } else {
-      return <div><h4>Something went wrong. Please reload the page</h4></div>;
-    }
-  }
-}
+const Home = () => (
+  <Row>
+    <Link to="/timer">
+      <img src={CloudyTimerButton} />
+    </Link>
+    <Link to="stopwatch">
+      <img src={CloudyStopwatchButton} />
+    </Link>
+  </Row>
+);
 
 export default App;
