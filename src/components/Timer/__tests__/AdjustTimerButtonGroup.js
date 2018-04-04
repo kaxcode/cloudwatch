dc1116207d7eb26d5bd4f2385dd09085007f2855
meingorn <max.eingorn@cloudreach.com>
2018-04-04 09:30:16 -0400

    Add tests for AdjustTimerButtonGroup component

---
+++ src/components/Timer/__tests__/AdjustTimerButtonGroup.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+import AdjustTimerButtonGroup from '../AdjustTimerButtonGroup';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Adjudst Timer Button Group', () => {
+  let wrapper;
+
+  const increaseHoursMock = jest.fn();
+  const decreaseHoursMock = jest.fn();
+  const increaseMinutesMock = jest.fn();
+  const decreaseMinutesMock = jest.fn();
+  const increaseSecondsMock = jest.fn();
+  const decreaseSecondsMock = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <AdjustTimerButtonGroup
+      increaseHours={increaseHoursMock}
+      decreaseHours={decreaseHoursMock}
+      increaseMinutes={increaseMinutesMock}
+      decreaseMinutes={decreaseMinutesMock}
+      increaseSeconds={increaseSecondsMock}
+      decreaseSeconds={decreaseSecondsMock}/>
+    );
+  });
+  it('renders correctly', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
