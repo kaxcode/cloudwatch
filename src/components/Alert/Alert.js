4b829c1c6ed420125a5bd45933e4452d8bddb512
meingorn <max.eingorn@cloudreach.com>
2018-04-04 15:11:39 -0400

    Update Alert component in Timer

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { func, string, bool } from 'prop-types';
 
+<<<<<<< HEAD
 export default class Alert extends React.Component {
   componentDidMount = () => {
     if (!this.props.show) {
@@ -25,9 +26,19 @@ export default class Alert extends React.Component {
     return null;
   }
 }
+=======
+const Alert = props => {
+  return props.show &&
+    <div onClick={props.onDismiss} style={{ textAlign: 'center', marginTop: '20px', backgroundColor: 'white', width: '100%', height: '100%', zIndex: 10000000, position: 'absolute'}}>
+      <img src={CloudyAlert} alt="cloud-shaped button to timer"/>
+    </div>;
+};
+>>>>>>> dcb2535... Update Alert component in Timer
 
 Alert.propTypes = {
   onDismiss: func.isRequired,
   msg: string,
   show: bool
 };
+
+export default Alert;
