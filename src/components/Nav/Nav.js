ac5af3bae63981f6c156c62b8fa8a426bc70a256
meingorn <max.eingorn@cloudreach.com>
2018-04-18 09:36:46 -0400

    Implement BEM, SVGO

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavCloud from '../../assets/blank_cloud.svg';
+import './Nav.css';
 
 const Nav = () => {
   return (
     <Link to="/">
-      <img className="nav" src={NavCloud} alt="Click to return home" />
+      <img className="Nav__Img" src={NavCloud} alt="Click to return home" />
     </Link>
   );
 };
